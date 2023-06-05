c86b2d5142f0e331f25dfe6f3de9276837024fc7
arisoda <38783514+arisoda@users.noreply.github.com>
2023-06-05 21:22:04 +0200

    gnome extension v1
    first gnome extension ever

---
+++ extension.js
@@ -0,0 +1,77 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/* exported init */
+
+
+const Main = imports.ui.main;
+const St = imports.gi.St;
+const Meta = imports.gi.Meta;
+
+function init() {
+  // Initialization code
+}
+
+function enable() {
+  let topPanel = Main.panel;
+  let topMonitor = global.display.get_primary_monitor(); // Get the primary monitor
+
+  // Attach the button-press-event to the top panel
+  topPanel.connect('button-press-event', function (widget, event) {
+    // Check if the middle mouse button was clicked
+    if (event.get_button() === 2) {
+      // Middle mouse click detected
+
+      // Get the stack of windows
+      let windows = global.display.get_tab_list(Meta.TabList.NORMAL_ALL, null);
+      
+      // Find the topmost maximized window on the same monitor as the top panel
+      let topmostMaximizedWindow = null;
+      let topmostWindowLayer = -1;
+
+      for (let i = 0; i < windows.length; i++) {
+        let window = windows[i];
+
+        // Check if the window is maximized and on the same monitor
+        if (
+          window.maximized_horizontally &&
+          window.maximized_vertically &&
+          window.get_monitor() === topMonitor
+        ) {
+          let windowLayer = window.get_layer();
+          
+          // Check if the window is at the top layer
+          if (windowLayer > topmostWindowLayer) {
+            topmostMaximizedWindow = window;
+            topmostWindowLayer = windowLayer;
+          }
+        }
+      }
+
+      // Hide the topmost maximized window on the same monitor, if found
+      if (topmostMaximizedWindow) {
+        topmostMaximizedWindow.minimize();
+      }
+    }
+  });
+}
+
+function disable() {
+  // Clean up code
+}
+
